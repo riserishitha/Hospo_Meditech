b4fb6337db6b195ceb966ce3fa5ee7f18a8fadab
Vinnu Gollakoti <vinnugollakoti@Vinnus-MacBook-Pro.local>
2024-09-24 16:25:45 +0530

    added tracking details frontend

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -5,21 +5,20 @@ const Navbar = () => {
       <div>
           <div className="nav-bar">
               {/* logo div */}
-              <div><p className="hospo-logo">hospo</p></div>
+              <Link to="/">
+              <div><p className="hospo-logo">hospo</p></div></Link>
               {/* middle buttons nav div  */}
               <div className="nav-bar-1">
-                  <Link to="/doctors"><p className="nav-btn">Doctors</p></Link> {/* {{ edit_2 }} */}
-                  <p className="nav-btn">About</p>
-                  <p className="nav-btn">Our services</p>
-                  <p className="nav-btn">Our team</p>
-                  <p className="nav-btn">Reviews</p>
-                  <p className="nav-btn">Contact</p>
+                  <Link to="/doctors"><p className="nav-btn">Doctors</p></Link>
+                  <Link to="/error"><p className="nav-btn">About</p></Link>
+                  <Link to="/error"><p className="nav-btn">Our&nbsp;services</p></Link>
+                  <Link to="/delivary"><p className="nav-btn">Delivery</p></Link>
+                  <Link to="/track"><p className="nav-btn">Track</p></Link>
+                  <Link to="/error"><p className="nav-btn">Contact</p></Link>
+                  
               </div>
               {/* get in touch div */}
               <div>
-                  <Link to="/"><button className="get-in-touch">Home</button></Link> {/* {{ edit_3 }} */}
-                  <Link to="/delivary"><button className="get-in-touch">Delivery</button></Link> {/* {{ edit_4 }} */}
-                  <Link to="/track"><button className="get-in-touch">Track</button></Link> {/* {{ edit_5 }} */}
                   <button className="get-in-touch">Get in touch</button>
               </div>
   
--- client/src/Components/Track.jsx
+++ client/src/Components/Track.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import Navbar from './NAvbar';
 import L, { icon } from 'leaflet';
 import io from 'socket.io-client';
 import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
@@ -37,20 +38,32 @@ function TrackDelivery() {
 
     return (
         <div>
-            <h1>Track Delivery Boy</h1>
-            {deliveryData ? (
+            <Navbar />
+            <div className='tracking-details-div'>
+            <div className='tracking-div'>
+                <div className='tracking-div-main'>
+                <center><p className='line2-1'>Tracking details</p>
+                <hr /><br />
+                <p className='line4-1'>Name : Vinay</p>
+                <p className='line4-1'>Phone no : +91 63011 81244</p>
+                <p className='line4-1'>Estimated time : 40 Min</p>
+                <p className='line4-1'>Payment mode : Cash on delivery</p>
+                </center>
+                </div>
                 <div>
-                    <p><strong>Name:</strong> {deliveryData.name}</p>
-                    <p><strong>Status:</strong> {deliveryData.status}</p>
-                    <p><strong>Location:</strong> Lat: {deliveryData.location.lat}, Lng: {deliveryData.location.lng}</p>
-                    
-                    <MapContainer center={mapCenter} zoom={13} style={{ height: '60vh', width: '100%' }}>
+                    <img src="https://res.cloudinary.com/dus9hgplo/image/upload/v1727173987/xh1hjjfnbrbm3qus4zjy.jpg" alt="" className='delivery-boy-image' />
+                </div>
+
+            </div>
+            {deliveryData ? (
+                <div className='tracking-map'>       
+                    <MapContainer center={mapCenter} zoom={28} className=' h-screen'>
                         <TileLayer
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                             attribution='Hospo'
                         />
                         <Marker position={[melat,melong]}icon={new icon({
-                            iconUrl: "https://www.clipartmax.com/png/middle/188-1882796_red-home-icon-house-icon-red.png",
+                            iconUrl: "https://res.cloudinary.com/dus9hgplo/image/upload/v1727150457/jdcxtpky1jw103pshbm5.png",
                             iconSize: [40,40], 
                             iconAnchor: [12, 41],
                         })}>
@@ -59,7 +72,7 @@ function TrackDelivery() {
                             </Popup>
                         </Marker>
                         <Marker position={[deliveryData.location.lat, deliveryData.location.lng]} icon={new icon({
-                            iconUrl: "https://www.creativehatti.com/wp-content/uploads/2021/10/Delivery-boy-is-going-on-scooter-for-pizza-delivery-5-small.jpg",
+                            iconUrl: "https://res.cloudinary.com/dus9hgplo/image/upload/v1727150160/pfauaxctuemmit6vwswf.png",
                             iconSize: [60, 71], 
                             iconAnchor: [12, 41],
                         })}>
@@ -72,6 +85,7 @@ function TrackDelivery() {
             ) : (
                 <p>Loading...</p>
             )}
+            </div>
         </div>
     );
 }
