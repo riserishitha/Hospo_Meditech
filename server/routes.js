c13ba8c7a474d2956cbf876f7e57483a9e849cfa
Mohana <mohanavamsi14@gmail.com>
2024-09-24 21:09:25 +0530

    done

--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,13 @@ const Medcard = require("./Medcard");
 const router=express.Router();
 const nodemailer = require("nodemailer");
 const send = require("./message");
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'mohanavamsi14@gmail.com', 
+        pass: 'wken sosx ofjc cqvu' 
+    }
+});
 
 router.get("/medcards",(req,res)=>{
     const data=Medcard.find({})
@@ -19,44 +26,38 @@ router.post("/register", async (req, res) => {
         date,
         doctorId} = req.body; 
         console.log(req.body)
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'mohanavamsi14@gmail.com', 
-            pass: 'wken sosx ofjc cqvu' 
-        }
-    });
-
+ 
     const mailOptions = {
         from: 'mohanavamsi14@gmail.com',
         to: doctorId,
         subject: 'New Registration',
-        html: `<h3>Hey doc your are having a booking on ${date} want to conform if ypu want to take the meting link provided below</h3>  <a href="https://hospo-fbdf.onrender.com/conform/${email}/${phone}">click this</a><a href="https://hospo.vercel.app/meeting/${email}">meeting link</a>`
+        html: `<h3>Hey doc your are having a booking on ${date} want to conform if ypu want to take the meting link provided below</h3>  <a href="https://hospo-fbdf.onrender.com/conform/${email}/${phone}">click this</a><b> And </b><a href="https://hospo.vercel.app/meeting/${email}">meeting link</a>`
     };
 
     try {
-        await send("your doctor will conform and send you the message",phone)
+        // await send("your doctor will conform and send you the message",phone)
         await transporter.sendMail(mailOptions);
+        await Medcard.create(req.body)
         res.status(200).json({ message: 'Email sent successfully' });
     } catch (error) {
         console.log(error)
         res.status(500).json({ error: 'Failed to send email' });
     }
 });
-router.post("/conform/:email/:phone", async (req, res) => {
+router.get("/conform/:email/:phone", async (req, res) => {
     const { email, phone } = req.params;
     try {
-        await Medcard.updateOne({ email, phone }, { status: 'confirmed' });
         const mailOptions = {
-            from: 'mohanavamsi16@outlook.com',
+            from: 'mohanavamsi14@gmail.com',
             to: email,
             subject: 'New Registration',
-            html: `Hey hi your schedule is done join meeting by clicking the lnk below on the scheduled date on 9:30 https://hospo-ten.vercel.app/meeting/${email}`
+            html: `Hey hi your schedule is done join meeting by clicking the lnk below on the scheduled date on 9:30 https://hospo-ten.vercel.app/meeting/${email.split("@")[0]}`
         };
         await transporter.sendMail(mailOptions);
-        await send(`Hey hi your schedule is done join meeting by clicking the lnk below on the scheduled date on 9:30 https://hospo-ten.vercel.app/meeting/${email}`,phone)
+        // await send(`Hey hi your schedule is done join meeting by clicking the lnk below on the scheduled date on 9:30 https://hospo-ten.vercel.app/meeting/${email}`,phone)
         res.status(200).json({ message: 'Booking confirmed successfully' });
     } catch (error) {
+        console.log(error)
         res.status(500).json({ error: 'Failed to confirm booking' });
     }
 });
